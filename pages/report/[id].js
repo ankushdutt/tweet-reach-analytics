229e376ed2710b5fab5404db0d403b6eeddf6704
Ankush Dutt <ankushd100@gmail.com>
2021-10-20 20:32:45 +0530

    Setup SSR in /report/[id].js

---
+++ pages/report/[id].js
@@ -0,0 +1,22 @@
+export default function Home({ data }) {
+  return (
+    <div>
+      <div>{console.log(data)}</div>
+    </div>
+  );
+}
+
+export async function getServerSideProps({ params }) {
+  const res = await fetch(
+    `https://api.twitter.com/2/tweets/${params.id}/retweeted_by`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: `${process.env.BEARER_TOKEN}`,
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  const data = await res.json();
+  return { props: { data } };
+}
